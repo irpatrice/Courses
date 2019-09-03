cc68da7b9959e871f555e4edb182786d3b38c465
Iryna <iryna_t@i.ua>
2019-09-04 01:22:05 +0300

    bootstrap

---
+++ Курсова/js/main.js
@@ -0,0 +1,12 @@
+// $('.lazy').slick({
+//     lazyLoad: 'ondemand',
+//     slidesToShow: 3,
+//     slidesToScroll: 1
+//   });
+
+// $(element).slick();
+
+
+$(document).ready(function(){
+    $('.slider').slick();
+  });
